a00bfbca0a87dab7903516e403a29fdaa1fc50b9
vioreld-VERTEX <76536573+vioreld-VERTEX@users.noreply.github.com>
2020-12-23 23:55:52 +0200

    23.12.2020

---
+++ ManagersSirMLD.cs
@@ -0,0 +1,55 @@
+﻿/*
+ 
+Metode de programare 
+
+Divide Et Impera (Prezentare Generala)
+
+cu Aplicatie:
+
+Crearea unei clase denumita 
+        "ManagerSirMLD" 
+care are urmatoarele proprietati: 
+
+    n=numarul de elemente reale ale sirului; 
+    min=elementul cu valoarea minima a sirului; 
+    max=elementul cu valoarea maxima a sirului; 
+    s[n]= sirul in sine; 
+
+si care are urmatoarele metode: 
+
+    Initializare (citire din fisier extern si evaluare proprietati initiale); 
+    Citire sir; (cu Divide-et-Impera)
+    Calculare Minim si Maxim; (cu Divide-et-Impera)
+    Evaluare erori; 
+    Cautare element in sir ordonat (cautare binara) (cu Divide-et-Impera);  
+    Ordonare sir crescator si descrescator; 
+    Evaluare stare ordonare sir; 
+
+Elemente invatate:
+    *Conceptul OOP (Object Oriented Programming - Programarea Orientata pe Obiecte)
+    *Conceptul de programare Divide et Impera
+    *
+*/
+
+using System;
+
+namespace ManagerSirMLD
+{
+    public class ManagersSirMLD
+    {
+        public int NumarElemente; //numarul de elemente al sirului - proprietate publica accesibila a obiectului (clasei) (in tema descris ca (n))
+
+        public double ElementMinim; //elementul cel mai mic al sirului - proprietate publica accesibila a obiectului (clasei) (in tema descris ca (min))
+
+        public double ElementMaxim; //elementul cel mai mare al sirului - proprietate publica sccesibila a obiectului (clasei) (in tema descris ca (max))
+
+        public double [] sir; // sirul de numere reale - proprietate publica accesibila a obiectului (clasei) (in tema descris ca s(n))
+        
+        public void Initializare() // functia care citeste dintr-un fisier tot datele de acolo. 
+        {
+            /*
+             *  Fisierul este un fisier .ini care contine setarile initiale ale programului. 
+             */
+        }
+    }
+}
